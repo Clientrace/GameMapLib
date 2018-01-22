eb8baff51aa0cc465b99fdcadddb70f206a0035b
DavusTanus <ChristianDave.Tan@gmail.com>
2018-01-22 21:00:06 +0800

    Improved Keypress handling

--- src/input/P_Keyboard_Input.java
+++ src/input/P_Keyboard_Input.java
@@ -4,11 +4,6 @@ package input;
 import org.lwjgl.input.Keyboard;
 
 
-
-
-
-
-
 /* JAVA LIBRARY */
 import java.util.ArrayList;
 
@@ -16,27 +11,26 @@ public class P_Keyboard_Input {
 	private static ArrayList<Integer> KeyInput;
 	private static Object[] KeypressArray;
 	
+	/* INITIALIZE KEYPRESS CONTAINER */
 	public static void initialize(){
 		KeyInput = new ArrayList<Integer>();
 	}
 	
-	public static void updateInput(){
-		
-		/* ADD KEYPRESS */
-		if(Keyboard.isKeyDown(Keyboard.getEventKey()) && !KeyInput.contains(Keyboard.getEventKey())){
-			KeyInput.add((Integer)Keyboard.getEventKey());
-		}
-		
-		/* REMOVE KEYPRESS */
-		KeypressArray = KeyInput.toArray();
-		
-		for(int a=0; a < KeypressArray.length; a++ ){
-			if(!Keyboard.isKeyDown((Integer)KeypressArray[a])){
-				KeyInput.remove(KeypressArray[a]);
-			}
+	/* REMOVE KEYPRESS AT RELEASE */
+	public static void removeKeypress(int key){
+		if(KeyInput.contains((Integer)key)){
+			KeyInput.remove((Integer)key);
 		}
 	}
 	
+	/* ADD KEYPRESS  AT PRESS */
+	public static void addKeypress(int key){
+		if(Keyboard.isKeyDown(key) && !KeyInput.contains(key)){
+			KeyInput.add((Integer)key);
+		}
+	}
+	
+	/* PRINTS ALL PRESSED KEYS TO CONSOLE */
 	public static void printControls(){
 		System.out.print("{ ");
 		for(Integer keyPressed : KeyInput){
@@ -46,7 +40,18 @@ public class P_Keyboard_Input {
 		
 	}
 	
-	public static boolean isPressed(int KeyIndex){
-		return KeyInput.contains((Integer)KeyIndex);
+	/* RETURNS AN ARRAY OF PRESSED KEYS */
+	public static Object[] getKeyList(){
+		return KeyInput.toArray();
+	}
+	
+	/* RETURNS TRUE IF ALL OF THE PARAMETERS ARE PRESSED */
+	public static boolean isPressed(int... args){
+		for(int a=0; a<args.length;a++){
+			if(!KeyInput.contains(args[a])){
+				return false;
+			}
+		}
+		return true;
 	}
 }
--- src/states/P_State_Menu.java
+++ src/states/P_State_Menu.java
@@ -14,7 +14,7 @@ import constants.C_STATE_ID;
 /* CONTROLS */
 import input.P_Keyboard_Input;
 
-public class P_State_Menu extends BasicGameState {
+public class P_State_Menu extends BasicGameState implements KeyListener{
 	/* ASSIGN STATE ID */
     public int getID() {
         return C_STATE_ID.MENU;
@@ -30,15 +30,31 @@ public class P_State_Menu extends BasicGameState {
     	graphics.drawString("<~ Menu", 34, 30);
         graphics.drawString("Keyboard Controls: (Test your Keyboard)", 50, 80);
         
-        if( P_Keyboard_Input.isPressed(Keyboard.KEY_SPACE) ){
-        	graphics.drawString("SPACE BAR", 100, 100);
+        Object[] KeyList = P_Keyboard_Input.getKeyList();
+        
+        for(int a=0; a<KeyList.length; a++){
+        	graphics.drawString(Keyboard.getKeyName((Integer)KeyList[a]), 100, a*25 + 100);
+        }
+        
+        if( P_Keyboard_Input.isPressed(Keyboard.KEY_SPACE, Keyboard.KEY_A, Keyboard.KEY_B) ){
+        	graphics.drawString("SPACE + A + B", 200, 200);
         }
         
     }
     
     /* UPDATE STATE VARIABLES */
     public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
-    	P_Keyboard_Input.updateInput();
     	P_Keyboard_Input.printControls();
+    }
+    
+    /* INPUT LISTENERS */
+    @Override
+    public void keyPressed(int key, char c) {
+    	P_Keyboard_Input.addKeypress(key);
+    }
+    
+    @Override
+    public void keyReleased(int key, char c) {
+    	P_Keyboard_Input.removeKeypress(key);
     }
 }
